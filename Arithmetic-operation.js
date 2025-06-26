b77ac8f934aa64b2339e1f2d27e3200bd75e79a4
NIKHIL PAL <nikhilpal9312@gmail.com>
2025-06-26 11:57:12 +0000

    container config done

---
+++ Arithmetic-operation.js
@@ -0,0 +1,8 @@
+// Arithmetic Operation
+let a = 10;
+let b = 2;
+console.log(a + b); //Arithmetic opeator,
+console.log(a - b);//subtraction opeator.
+console.log(a * b);// multiplication operator.
+console.log(a / b)//division operator.
+console.log(a % b)//mudules 
