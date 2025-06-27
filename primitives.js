56b1960faac7037b4b01ad4b28f5ae13b89c128c
nikhil pal <nikhilpal9312@gmail.com>
2025-06-27 12:16:48 +0530

    container config done

---
+++ primitives.js
@@ -0,0 +1,13 @@
+//String, symbol , null , number, bollean, bigint , undefined
+let a = "helloworld";
+let b = Symbol("this is a symbol");
+let c = null;
+let d = 12;
+let e = true;
+let f = BigInt("321") + BigInt("9");
+let g = undefined;
+console.log (a , b , c , d , e , f , g)
+
+
+
+
