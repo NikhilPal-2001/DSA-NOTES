9191c0cbbe5a507fd594e33e4115cbb5def9dada
nikhil pal <nikhilpal9312@gmail.com>
2025-06-27 12:47:33 +0530

    container config done

---
+++ condition-statement.js
@@ -0,0 +1,9 @@
+let a = prompt("enter the number = ");
+a = Number.parseInt(a) // converting the string to number, 
+if ( a % 2 == 0){
+    console.log("This is even number = ",a)
+} 
+else 
+{
+    console.log("This is odd number = ", a);
+}                              
