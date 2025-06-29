5443889f15257195cd01d6f91890fe46073036e0
nikhil pal <nikhilpal9312@gmail.com>
2025-06-29 11:21:41 +0530

    container config done

---
+++ String-Reverse.js
@@ -0,0 +1,14 @@
+let str = " Ramesh"
+// length of string 
+// console.log(str.length)
+
+
+
+// reverse string 
+let rev = "";
+for (let i = str.length-1; i >= 0 ; i--)
+{
+    rev = rev + str[i];
+    
+}
+console.log(rev)
