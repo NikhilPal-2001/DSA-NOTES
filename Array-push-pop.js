aca2f3a52132fb2c0e06c04b2c86af11b02e0414
nikhil pal <nikhilpal9312@gmail.com>
2025-07-03 12:30:22 +0530

    container config done

--- Array-push-pop.js
+++ Array-push-pop.js
@@ -1,13 +1,26 @@
 let Arr = [1,2,3,4,5]
 // console.log(Arr) [1,2,3,4,5]
 
-// push operation
+// push operation 
+//push operation is perform Add(push) value End. 
 Arr.push(6,7,8,9); //[1,2,3,4,5] push = [1,2,3,4,5,6,7,8,9]
 // console.log(Arr) //
 
 // pop Operation 
+//push operation is perform delete(pop) value End. 
 Arr.pop() //[1,2,3,4,5,6,7,8,9] pop = [1,2,3,4,5,6,7,8]
 Arr.pop() //[1,2,3,4,5,6,7,8] pop = [1,2,3,4,5,6,7]
 Arr.pop() //[1,2,3,4,5,6,7] pop = [1,2,3,4,5,6]
-console.log(Arr) //output [1,2,3,4,5,6]
+// console.log(Arr) //output [1,2,3,4,5,6]
+ 
+// unshift operation 
+// Unshift operation is perform add value to start
+Arr.unshift(0);//[1,2,3,4,5,6] = [0,1,2,3,4,5,6]
+Arr.unshift(10);//[0,1,2,3,4,5,6] = [10,0,1,2,3,4,5,6]
+// console.log(Arr); //[10,0,1,2,3,4,5,6]
+
+// shift operation
+// shift Operation is perform is 
+Arr.shift()
+console.log(Arr);
 
