76c5c1303908d62d1e1349ae587f9579912ae033
nikhil pal <nikhilpal9312@gmail.com>
2025-07-03 12:14:50 +0530

    container config done

---
+++ Array-push-pop.js
@@ -0,0 +1,13 @@
+let Arr = [1,2,3,4,5]
+// console.log(Arr) [1,2,3,4,5]
+
+// push operation
+Arr.push(6,7,8,9); //[1,2,3,4,5] push = [1,2,3,4,5,6,7,8,9]
+// console.log(Arr) //
+
+// pop Operation 
+Arr.pop() //[1,2,3,4,5,6,7,8,9] pop = [1,2,3,4,5,6,7,8]
+Arr.pop() //[1,2,3,4,5,6,7,8] pop = [1,2,3,4,5,6,7]
+Arr.pop() //[1,2,3,4,5,6,7] pop = [1,2,3,4,5,6]
+console.log(Arr) //output [1,2,3,4,5,6]
+
