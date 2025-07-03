264923009b1d0fa78b6a6254994fbbe569822b7b
nikhil pal <nikhilpal9312@gmail.com>
2025-07-03 12:02:59 +0530

    container config done

---
+++ SumofArray.js
@@ -0,0 +1,13 @@
+function ArrayQuestion(Arr)
+{
+    let sum = 0;
+    for (let i = 0 ; i <= Arr.length ;i++)
+    {
+         sum = sum +i ;
+        
+    }
+
+    console.log (sum);
+}
+Arr = [10,9,8,7,7,7,5,5]
+ArrayQuestion(Arr);
---
+++ array.js
@@ -0,0 +1,8 @@
+// Array is collection of items .
+// define Array
+Arr = ["nikhil", "shubham","ashish kumar", "tushar Sharma","Aditya Rawat"]
+// console.log(Arr[0]);
+console.log(Arr);
+console.log(Arr.length);
+
+
