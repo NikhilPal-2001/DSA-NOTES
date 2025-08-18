d53a1e29d007b75c300268ea17291c52476d7b2a
nikhil pal <nikhilpal9312@gmail.com>
2025-08-18 12:30:56 +0530

    Create Date_And_Time.js

---
+++ Date_And_Time.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+// console.log(myDate)
+
+
+// connvert to string 
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+console.log(myDate.toLocaleDateString())
+// console.log(myDate.toDateString())
